6710c8abb0d4fa9b4934048c6ff86c1bf72ad8db
driversline <n526258360@gmail.com>
2024-12-26 17:35:49 +0300

    react.sh

--- react/react.sh
+++ react/react.sh
@@ -1,1 +1,23 @@
-#!bin/bash
+#!/bin/bash
+
+if ! command -v node &> /dev/null; then
+    echo "Installing node and npm..."
+    sudo pacman -S --noconfirm nodejs npm
+else
+    echo "Node and npm are already installed."
+fi
+
+read -p "Enter the path (e.g., /home/user/my-react-app): " projectPath
+
+mkdir -p "$projectPath"
+cd "$projectPath" || exit
+
+npm init -y
+
+npm install react react-dom react-router-dom
+
+mkdir -p src public
+
+echo "Excellent."
+
+exit 0
